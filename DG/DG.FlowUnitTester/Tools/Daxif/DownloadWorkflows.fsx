bbdeb00ba4f6f1e377176b87e835bea8b52e3963
Thyge Skoedt Steffensen <31892312+thygesteffensen@users.noreply.github.com>
2021-01-26 10:28:57 +0100

    Refactored DownloadWorkflows.fsx

--- DG/DG.FlowUnitTester/Tools/Daxif/DownloadWorkflows.fsx
+++ DG/DG.FlowUnitTester/Tools/Daxif/DownloadWorkflows.fsx
@@ -2,15 +2,16 @@
 Download flows from given solution
 *)
 
-#load @"_Config.fsx"
+#load "_Config.fsx"
+
 open _Config
 open DG.Daxif
 open DG.Daxif.Common.Utility
 open System.IO.Compression
 
 #if INTERACTIVE
-#r @"System.IO.Compression"
-#r @"System.IO.Compression.FileSystem"
+#r "System.IO.Compression"
+#r "System.IO.Compression.FileSystem"
 #endif
 open System.IO
 open System.Net
@@ -19,34 +20,41 @@ open System.IO.Compression
 let workflowPath = Path.solutionRoot ++ @"Tests\Workflows\"
 let flowSolutionName = "FlowUnitTester"
 
-let ExtractFlowsFromSolution =    
+let ExtractFlowsFromSolution =
     let EmptyFolder =
-        Directory.GetFiles(workflowPath , "*.*", SearchOption.AllDirectories) |> Array.iter (fun file -> File.Delete(file))
+        Directory.GetFiles(workflowPath, "*.*", SearchOption.AllDirectories)
+        |> Array.iter (fun file -> File.Delete(file))
 
     let ExtractAndUnzip =
         EmptyFolder
-        Solution.Export(Env.dev, flowSolutionName, workflowPath , false, true) |> ignore
-        use archive = ZipFile.Open(workflowPath + flowSolutionName + ".zip", ZipArchiveMode.Read)
-        
+
+        Solution.Export(Env.dev, flowSolutionName, workflowPath, false, true)
+        |> ignore
+
+        use archive =
+            ZipFile.Open(workflowPath + flowSolutionName + ".zip", ZipArchiveMode.Read)
+
         archive.ExtractToDirectory(workflowPath)
-        
+
         archive.Dispose
-    
+
     let OrganizeFiles =
         let moveOutOfSubFolder file =
             let fileName = Path.GetFileName(file)
             File.Move(file, workflowPath + "/" + fileName)
-        
-        let deleteNonFlows file = 
+
+        let deleteNonFlows file =
             match file with
-            | file when file.ToString().EndsWith(".json") -> printf "Keeping: %s\n" file
-                                                             moveOutOfSubFolder file
-            //| file when file.ToString().EndsWith(".zip") -> printf "Keeping: %s\n" file
-            | _ -> printf "Deleting: %s\n" file
-                   File.Delete(file)
-    
-        Directory.GetFiles(workflowPath , "*.*", SearchOption.AllDirectories) |> Array.iter deleteNonFlows
+            | file when file.ToString().EndsWith(".json") ->
+                  printf "Keeping: %s\n" file
+                  moveOutOfSubFolder file
+            | _ ->
+                printf "Deleting: %s\n" file
+                File.Delete(file)
+
+        Directory.GetFiles(workflowPath, "*.*", SearchOption.AllDirectories)
+        |> Array.iter deleteNonFlows
+
         Directory.Delete(workflowPath + "/Workflows")
 
     OrganizeFiles
-
